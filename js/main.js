c84e581a33f16176c834925aa1af1b5757bb2310
orlandobedabest <thecreeperboi21@gmail.com>
2024-06-25 17:00:36 +0000

    Copied Code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $( document ).ready(function() {
     $('#btn').click(function() {
-        window.alert("DID YOU JUST CLICK ME?!");
+        window.alert("!YOU JUST GOT A VIRUS! Press OK to install cleaner");
+    });
+});
+$( document ).ready(function() {
+    $('#bttn').click(function() {
+        window.alert("!YOU JUST WON ONE MILLION DOLLARS! Press OK to claim");
     });
 });
