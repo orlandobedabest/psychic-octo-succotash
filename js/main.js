8e766c839844c320c132a716d9ba900174a8bfcd
orlandobedabest <thecreeperboi21@gmail.com>
2024-06-26 16:19:24 +0000

    did mini assignment

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ $( document ).ready(function() {
         window.alert("!YOU JUST GOT A VIRUS! Press OK to install cleaner");
     });
 });
-$( document ).ready(function() {
-    $('#bttn').click(function() {
-        window.alert("!YOU JUST WON ONE MILLION DOLLARS! Press OK to claim");
-    });
-});
+//$( document ).ready(function() {
+  //  $('#bttn').click(function() {
+    //    window.alert("!YOU JUST WON ONE MILLION DOLLARS! Press OK to claim");
+ //   });
+//});
